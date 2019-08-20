c97bc11c7d402287fff69673412cdef151fefb47
Dr Wolf ML <wolfgangml@mac.com>
2019-08-20 12:48:41 +1000

    added deployment scipts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t superclass2016/multi-client:latest -t superclass2016/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t superclass2016/multi-server:latest -t superclass2016/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t superclass2016/multi-worker:latest -t superclass2016/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push superclass2016/multi-client:latest
+docker push superclass2016/multi-server:latest
+docker push superclass2016/multi-worker:latest
+
+docker push superclass2016/multi-client:$SHA
+docker push superclass2016/multi-server:$SHA
+docker push superclass2016/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set deployments/server-deployment server=superclass2016/multi-server:$SHA
+kubectl set deployments/server-deployment server=superclass2016/multi-server:$SHA
+kubectl set deployments/server-deployment server=superclass2016/multi-server:$SHA
